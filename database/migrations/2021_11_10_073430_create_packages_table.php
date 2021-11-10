efab2574e14e9a44582aaa0422f5b928c7646742
DarioDa86 <dario.dagnelli@gmail.com>
2021-11-10 09:09:09 +0100

    first commit

---
+++ database/migrations/2021_11_10_073430_create_packages_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePackagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('packages', function (Blueprint $table) {
+            $table->id();
+            $table->string("destination", 30);
+            $table->date("departures");
+            $table->tinyInteger("duration");
+            $table->decimal("price", 5, 2);
+            $table->string("name company", 30);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('packages');
+    }
+}
